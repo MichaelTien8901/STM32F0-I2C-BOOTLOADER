5d06b23ce451d781d0d5b7a5025cfc887f4326bb
Michael Tien <mtien888@gmail.com>
2016-11-30 13:15:56 -0800

    Update dummy_main vector table to prevent warning of extended constant intialiser used.

--- zboot0/Src/dummy_main.c
+++ zboot0/Src/dummy_main.c
@@ -4,8 +4,10 @@
 // put the dummy_vector_table to application_address 08003000
 //
 extern void dummy_main_function(void);
-uint32_t const dummy_vector_table[2] = {
-  0x200004e8, 
-  (uint32_t) dummy_main_function // 
+typedef void (*pfunc)(void);
+pfunc const dummy_vector_table[2] = {
+  (pfunc) 0x200004e8, // stack
+//  (uint32_t) dummy_main_function // extended constant initialiser used warning??
+  dummy_main_function
 };
 
